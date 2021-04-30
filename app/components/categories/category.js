c3cf3fc1b37248dba8f7b5154fcdce6573c2795e
Agathe Badia <ab@honeypot.io>
2021-04-30 18:17:56 +0200

    WIP, display each category's name and description when clicking on category

--- app/components/categories/category.js
+++ app/components/categories/category.js
@@ -1,5 +1,15 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class CategoryComponent extends Component {
+  @service store;
+  @tracked category = {};
 
+  // protanopia = computed('category', function(){
+  // if (this.get('category.name') === 'protanopia') {
+  //   return true;
+  // }
+  // })
 }
