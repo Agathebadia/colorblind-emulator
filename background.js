5f951d97f342b65932a311e663861665e0410f54
Agathe Badia <ab@honeypot.io>
2021-07-27 16:09:10 +0200

    WIP display key and value from chrome.storage.sync

--- background.js
+++ background.js
@@ -1,6 +1,32 @@
-let color = '#3aa757';
+// let color = '#3aa757';
+let key = 'myKey';
+let value = { name: 'my value' };
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.local.set({key: value});
+//   console.log('Stored name:' + value.name);
+//   chrome.storage.local.get([key], (result) => {
+//     if (chrome.runtime.lastError)
+//         console.log('Error getting');
+
+//     console.log('Retrieved name: ' + result.myKey.name);
+// });
+// });
+
+chrome.storage.sync.set({key: value}, function() {
+  console.log('Value is set to ' + key[value]);
+});
+
+chrome.storage.sync.get(['key'], function(result) {
+  console.log('Value currently is ' + result.key);
+});
+
+
+chrome.runtime.onInstalled.addListener(function (changes, namespace) {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
 });
--- colorblindListener.js
+++ colorblindListener.js
@@ -1,40 +1,60 @@
 window.selectedFilter = null;
 
-window.onload = function() {
-  if (!chrome || !chrome.storage || !chrome.storage.local) return;
-  chrome.storage.local.get(["key"], function(result) {
-    try {
-      document.getElementById(result.key).click();
-    } catch (e) {
-      console.log(e);
-    }
-  });
-};
+// window.onload = function() {
+//   if (!chrome || !chrome.storage || !chrome.storage.local) return;
+//   chrome.storage.local.get(["key"], function(result) {
+//     try {
+//       document.getElementById(result.key).click();
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   });
+// };
+const key = 'myKey';
+const value = { name: 'my value' };
 
+chrome.storage.local.set({key: value}, () => {
+  if (chrome.runtime.lastError)
+        console.log('Error setting');
+
+  console.log('Stored name: ' + value.name);
+});
+
+const key = 'myKey';
+chrome.storage.local.get([key], (result) => {
+  if (chrome.runtime.lastError)
+        console.log('Error getting');
+
+  console.log('Retrieved name: ' + result.myKey.name);
+});
+
+chrome.storage.local.clear(() => {
+    console.log('Everything was removed');
+});
 /**
  * Sets the selected filter in storage
 //  * param {String} value the selected input
  */
 
-function setSelected(value) {
-  try {
-    chrome.storage.local.set({ key: value }, function() {
-      document.getElementById(value).checked = true;
-    });
-  } catch {}
-}
+// function setSelected(value) {
+//   try {
+//     chrome.storage.local.set({ key: value }, function() {
+//       document.getElementById(value).checked = true;
+//     });
+//   } catch {}
+// }
 
-function injectFilter(fileName) {
-  chrome.tabs.executeScript({ file: fileName });
-}
+// function injectFilter(fileName) {
+//   chrome.tabs.executeScript({ file: fileName });
+// }
 
-document.querySelectorAll(['[id^="radio"]']).forEach(radioButton => {
-  const filter = radioButton.parentElement.id.replace("option-", "");
-  radioButton.addEventListener("click", function() {
-    // page-specific filters
-    setSelected(radioButton.id);
-    injectFilter(`filters/${filter}.js`);
-    // popup-specific filters
-    // applyFilter((window.selectedFilter = filter));
-  });
-});
+// document.querySelectorAll(['[id^="radio"]']).forEach(radioButton => {
+//   const filter = radioButton.parentElement.id.replace("option-", "");
+//   radioButton.addEventListener("click", function() {
+//     // page-specific filters
+//     setSelected(radioButton.id);
+//     injectFilter(`filters/${filter}.js`);
+//     // popup-specific filters
+//     // applyFilter((window.selectedFilter = filter));
+//   });
+// });
