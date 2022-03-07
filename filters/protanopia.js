5134cce710789130ea2b81618f3b78e3afdfbf08
Agathe Badia <ab@honeypot.io>
2022-03-07 21:42:06 +0100

    Remove hover.js

--- filters/protanopia.js
+++ filters/protanopia.js
@@ -1,10 +1,32 @@
-if (document.getElementById("option-protanopia")) {
-    // stylingID = document.getElementById("styleID612481").remove();
+// if (document.getElementById("filterID471924")) {
+//     // stylingID = document.getElementById("styleID612481").remove();
+//     filterID = document.getElementById("filterID471924").remove();
+// }
+// // stylingID = document.createElement('style');
+// // stylingID.id = "styleID612481";
+// // document.body.appendChild(stylingID);
+
+// filterID = document.createElement('div');
+// filterID.id = "filterID471924";
+// filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
+// document.body.appendChild(filterID);
+
+// filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia"> <feColorMatrix type="matrix" values="0.567,0.433,0,0,0, 0.558,0.442,0,0,0, 0,0.242,0.758,0,0,0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+// // stylingID.innerHTML = 'html{-webkit-filter:url(#protanopia);-moz-filter:(#protanopia);-ms-filter:(#protanopia);-o-filter:(#protanopia);filter:(#protanopia);}'
+// setTimeout(function() {
+//     window.scrollBy(1, 1);
+//     window.scrollBy(-1, -1);
+// }, 1);
+
+// protanopia
+
+if (document.getElementById("styleID612481")) {
+    stylingID = document.getElementById("styleID612481").remove();
     filterID = document.getElementById("filterID471924").remove();
 }
-// stylingID = document.createElement('style');
-// stylingID.id = "styleID612481";
-// document.body.appendChild(stylingID);
+stylingID = document.createElement('style');
+stylingID.id = "styleID612481";
+document.body.appendChild(stylingID);
 
 filterID = document.createElement('div');
 filterID.id = "filterID471924";
@@ -12,7 +34,7 @@ filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0
 document.body.appendChild(filterID);
 
 filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia"> <feColorMatrix type="matrix" values="0.567,0.433,0,0,0, 0.558,0.442,0,0,0, 0,0.242,0.758,0,0,0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
-// stylingID.innerHTML = 'html{-webkit-filter:url(#protanopia);-moz-filter:(#protanopia);-ms-filter:(#protanopia);-o-filter:(#protanopia);filter:(#protanopia);}'
+stylingID.innerHTML = 'html{-webkit-filter:url(#protanopia);-moz-filter:(#protanopia);-ms-filter:(#protanopia);-o-filter:(#protanopia);filter:(#protanopia);}'
 setTimeout(function() {
     window.scrollBy(1, 1);
     window.scrollBy(-1, -1);
